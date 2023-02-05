955cd74f58ce70ce25887f535bb993943990179c
Daniil Shipilin <daniil.shipilin@gmail.com>
2023-02-05 13:21:37 +0100

    Don't sign non project assemblies

--- OTPManager.Wpf/SignAndCreatePackage.bat
+++ OTPManager.Wpf/SignAndCreatePackage.bat
@@ -12,9 +12,6 @@ signtool.exe timestamp /tr "%timestamp%" /td sha256 "%src%\OTPManager.exe"
 signtool.exe sign /fd sha256 /a /f "%cert%" "%src%\OTPManager.dll"
 signtool.exe timestamp /tr "%timestamp%" /td sha256 "%src%\OTPManager.dll"
 
-signtool.exe sign /fd sha256 /a /f "%cert%" "%src%\ApplicationUpdater.dll"
-signtool.exe timestamp /tr "%timestamp%" /td sha256 "%src%\ApplicationUpdater.dll"
-
 del /S /Q "%bin%\OTPManager.zip" >nul 2>&1
 7za.exe a -mx0 -tzip "%bin%\OTPManager.zip" "%src%\*"
 7za.exe h -scrcSHA256 "%bin%\OTPManager.zip" > "%bin%\sha256.txt"
