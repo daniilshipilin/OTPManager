b379f4afa20ef3671edc783a00ecb48acee7500c
Daniil Shipilin <daniil.shipilin@gmail.com>
2023-11-28 20:30:33 +0100

    ApplicationUpdater lib version up

--- OTPManager.Wpf/Build.bat
+++ OTPManager.Wpf/Build.bat
@@ -1,7 +1,7 @@
 @echo off
 
-del /S /Q .\bin\Release\publish >nul 2>&1
-rmdir /S /Q .\bin\Release\publish >nul 2>&1
+del /S /Q .\bin\publish >nul 2>&1
+rmdir /S /Q .\bin\publish >nul 2>&1
 
-dotnet publish -c Release -v m -o .\bin\Release\publish
+dotnet publish -c Release -v m -o .\bin\publish
 pause
--- OTPManager.Wpf/SignAndCreatePackage.bat
+++ OTPManager.Wpf/SignAndCreatePackage.bat
@@ -3,8 +3,7 @@
 set "cert=%CodesignCertPath%"
 set "timestamp=http://timestamp.digicert.com"
 
-set "bin=.\bin"
-set "src=%bin%\Release\publish"
+set "src=.\bin\publish"
 
 signtool.exe sign /fd sha256 /a /f "%cert%" "%src%\OTPManager.exe"
 signtool.exe timestamp /tr "%timestamp%" /td sha256 "%src%\OTPManager.exe"
@@ -12,8 +11,8 @@ signtool.exe timestamp /tr "%timestamp%" /td sha256 "%src%\OTPManager.exe"
 signtool.exe sign /fd sha256 /a /f "%cert%" "%src%\OTPManager.dll"
 signtool.exe timestamp /tr "%timestamp%" /td sha256 "%src%\OTPManager.dll"
 
-del /S /Q "%bin%\OTPManager.zip" >nul 2>&1
-7za.exe a -mx0 -tzip "%bin%\OTPManager.zip" "%src%\*"
-7za.exe h -scrcSHA256 "%bin%\OTPManager.zip" > "%bin%\sha256.txt"
+del /S /Q ".\bin\OTPManager.zip" >nul 2>&1
+7za.exe a -mx0 -tzip ".\bin\OTPManager.zip" "%src%\*" -xr!*.pdb
+7za.exe h -scrcSHA256 ".\bin\OTPManager.zip" > ".\bin\sha256.txt"
 
 pause
