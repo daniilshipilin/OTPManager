126665b3f0e56486fe9f61bddcbab863d67ee9ad
Daniil Shipilin <daniil.shipilin@gmail.com>
2025-07-15 21:17:31 +0200

    Removed ApplicationUpdater dependency

--- OTPManager.Wpf/App.xaml.cs
+++ OTPManager.Wpf/App.xaml.cs
@@ -1,9 +1,7 @@
 namespace OTPManager.Wpf;
 
 using System;
-using System.Threading.Tasks;
 using System.Windows;
-using ApplicationUpdater;
 using OTPManager.Wpf.Helpers;
 using OTPManager.Wpf.Views;
 
@@ -41,9 +39,6 @@ public partial class App : Application
             Environment.Exit(0);
         }
 
-        // check for updates in the background
-        var updateTask = Task.Run(CheckUpdates);
-
         while (true)
         {
             var loginView = new LoginView();
@@ -60,56 +55,6 @@ public partial class App : Application
             }
         }
 
-        if (!updateTask.IsCompleted)
-        {
-            updateTask.Wait();
-        }
-
         Environment.Exit(0);
-    }
-
-    private static async Task CheckUpdates()
-    {
-        if ((DateTime.Now - AppSettings.UpdatesLastCheckedTimestamp).Days >= 1)
-        {
-            try
-            {
-                var updater = new Updater(
-                ApplicationInfo.BaseDirectory,
-                ApplicationInfo.AppVersion,
-                ApplicationInfo.AppGUID,
-                ApplicationInfo.ExePath);
-
-                AppSettings.UpdateUpdatesLastCheckedTimestamp();
-
-                if (await updater.CheckUpdateIsAvailable())
-                {
-                    var dr = MessageBox.Show(
-                        updater.GetUpdatePrompt(),
-                        "Program update",
-                        MessageBoxButton.YesNo,
-                        MessageBoxImage.Information);
-
-                    if (dr == MessageBoxResult.Yes)
-                    {
-                        await updater.Update();
-                        Environment.Exit(0);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                ShowExceptionMessage(ex);
-            }
-        }
-    }
-
-    private static void ShowExceptionMessage(Exception ex)
-    {
-        MessageBox.Show(
-            ex.Message,
-            ex.GetType().ToString(),
-            MessageBoxButton.OK,
-            MessageBoxImage.Error);
     }
 }
