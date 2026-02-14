518270de783e5b9b558ffcdc3125a90ef1474873
Daniil Shipilin <daniil.shipilin@gmail.com>
2026-02-14 10:38:31 +0100

    .NET 10 migration

--- OTPManager.Wpf/ApplicationInfo.cs
+++ OTPManager.Wpf/ApplicationInfo.cs
@@ -1,8 +1,6 @@
 namespace OTPManager.Wpf;
 
 using System;
-using System.IO;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
 
@@ -10,9 +8,6 @@ public static class ApplicationInfo
 {
     private static readonly Assembly Ass = Assembly.GetExecutingAssembly();
     private static readonly AssemblyTitleAttribute? Title = Ass.GetCustomAttribute<AssemblyTitleAttribute>();
-    private static readonly AssemblyProductAttribute? Product = Ass.GetCustomAttribute<AssemblyProductAttribute>();
-    private static readonly AssemblyDescriptionAttribute? Description = Ass.GetCustomAttribute<AssemblyDescriptionAttribute>();
-    private static readonly AssemblyCopyrightAttribute? Copyright = Ass.GetCustomAttribute<AssemblyCopyrightAttribute>();
 
     public const string AppBuild =
 #if DEBUG
@@ -21,38 +16,11 @@ public static class ApplicationInfo
         "";
 #endif
 
-    public static IList<string>? Args { get; private set; }
-
-    public static string BaseDirectory => Path.GetDirectoryName(ExePath) ?? string.Empty;
-
     public static string ExePath { get; } = Process.GetCurrentProcess().MainModule?.FileName ?? string.Empty;
 
     public static string AppTitle { get; } = Title?.Title ?? string.Empty;
 
-    public static string AppProduct { get; } = Product?.Product ?? string.Empty;
-
     public static string AppHeader => $"{AppTitle} v{AppVersion.ToString(3)}{AppBuild}";
 
     public static Version AppVersion { get; } = Ass.GetName().Version!;
-
-    public static string AppAuthor { get; } = Copyright?.Copyright ?? string.Empty;
-
-    public static string AppDescription { get; } = Description?.Description ?? string.Empty;
-
-    public static Guid AppGUID { get; } = new Guid("c8f7186f-6e1b-4e62-a821-7278cf94b915");
-
-    /// <summary>
-    /// Gets application info formatted string.
-    /// </summary>
-    public static string AppInfoFormatted =>
-        $"{AppHeader}{Environment.NewLine}" +
-        $"{AppVersion}{Environment.NewLine}" +
-        $"Author: {AppAuthor}{Environment.NewLine}{Environment.NewLine}" +
-        $"Description:{Environment.NewLine}" +
-        $"  {AppDescription}";
-
-    /// <summary>
-    /// Sets application command line arguments.
-    /// </summary>
-    public static void SetArgs(string[] args) => Args = [.. args];
 }
